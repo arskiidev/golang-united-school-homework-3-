cf3f81a01b105285b66f573d741f3cf5e92823fb
hypnospaced <hypnospaced@yandex.ru>
2022-05-16 14:22:15 +0400

    Homework

--- task02-slice.go
+++ task02-slice.go
@@ -1,7 +1,7 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	for i := 1; i < len(input); i++ {
+	for i := 1; i <= len(input); i++ {
 		result = append(result, input[len(input)-i])
 	}
 	return result
